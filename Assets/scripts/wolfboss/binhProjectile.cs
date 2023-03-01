00362b1ef7fb73a44679381da416727b1a38987a
HieuTriTue <thailehieu@gmail.com>
2023-03-01 22:03:25 +0700

    feat: hoàn thiện boss logic

--- Assets/scripts/wolfboss/binhProjectile.cs
+++ Assets/scripts/wolfboss/binhProjectile.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class binhProjectile : MonoBehaviour
 {
-    
+
     void Update()
-    {
+    {   
         if(transform.position.y<=-14f) Destroy(gameObject);
         transform.Translate(new Vector2(3f,-2f)*Time.deltaTime);
     }
