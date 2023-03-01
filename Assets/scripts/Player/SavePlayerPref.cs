d4dfffd61c4272790753a0028c3357b95d0c0e53
DatHoang89 <datvip1938@gmail.com>
2023-03-01 22:43:24 +0700

    add time played counting

--- Assets/scripts/Player/SavePlayerPref.cs
+++ Assets/scripts/Player/SavePlayerPref.cs
@@ -13,21 +13,28 @@ public class SavePlayerPref : MonoBehaviour
           
       }
 
-      public static void IncreaseDeath()
-      {
-           int deathNum = PlayerPrefs.GetInt("deathNum");
-           deathNum++;
-           PlayerPrefs.SetInt("deathNum",deathNum);
-           Debug.Log("New Death Score:" + PlayerPrefs.GetInt("deathNum")); 
-      }
-      
-      public static void Load()
-      {
-          
-      }
+    public static void SaveTimePlayed(float timePlayed)
+    {
+        PlayerPrefs.SetFloat("Level3_TimePlayed", timePlayed);
+    }
 
-      public static void Reset()
-      {
-          PlayerPrefs.DeleteAll();
-      }
+    public static void IncreaseDeath()
+    {
+        int deathNum = PlayerPrefs.GetInt("deathNum");
+        deathNum++;
+        PlayerPrefs.SetInt("deathNum", deathNum);
+        Debug.Log("New Death Score:" + PlayerPrefs.GetInt("deathNum"));
+
+        // Lưu thời gian đã chơi khi người chơi chết
+        float timePlayed = Time.time - PlayerPrefs.GetFloat("Level3_TimeStarted");
+        Debug.Log("Time played: " + timePlayed);
+        SaveTimePlayed(timePlayed);
+    }
+
+
+    public static void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
 }
