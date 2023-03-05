c86fa52f94fd6fa9f26a3e177a1ab686473b9ae2
HieuTriTue <thailehieu@gmail.com>
2023-03-05 22:38:34 +0700

    feat: deathcount+endgame UI

--- Assets/scripts/UI/Dialog.cs
+++ Assets/scripts/UI/Dialog.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 
 
 public class Dialog : MonoBehaviour
-{
+{   
+    [SerializeField] AudioSource typeSFX;
+    [SerializeField] AudioSource winSFX;
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
     public float typingSpeed;
@@ -14,10 +16,16 @@ public class Dialog : MonoBehaviour
 
     public GameObject continueButton;
     public GameObject EGImage;
+    [SerializeField] TextMeshProUGUI deathCount;
+    [SerializeField] GameObject deathCountObject;
+    int sentenceCount=0;
+    bool ended=false;
+    float timer=0;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Type());
+        deathCount.text = PlayerPrefs.GetInt("deathNum").ToString() + " lần";
     }
 
     // Update is called once per frame
@@ -27,14 +35,30 @@ public class Dialog : MonoBehaviour
         {
             continueButton.SetActive(true);
         }
+        if(ended)
+        {
+            timer+=Time.deltaTime;
+            if(timer>=10f)
+                {
+                    SavePlayerPref.Reset();
+                    Application.Quit();
+                    Debug.Log("quit");
+                }
+        }
     }
     IEnumerator Type()
-    {
+    {   typeSFX.Play();
         foreach (char c in sentences[index].ToCharArray())
         {
             textDisplay.text +=c;
             yield return new WaitForSeconds(typingSpeed);
         }
+        typeSFX.Stop();
+        if(sentenceCount==1)
+        {
+             deathCountObject.SetActive(true);
+        }
+        sentenceCount++;
     }
     public void NextSentence()
     {
@@ -43,13 +67,16 @@ public class Dialog : MonoBehaviour
             index++;
             textDisplay.text = "";
             StartCoroutine(Type());
+           
         }
         else 
         {
             textDisplay.text = "";
             continueButton.SetActive(false);
             EGImage.SetActive(true);
+            winSFX.Play();
             Debug.Log("Image enabled");
+            ended=true;
         }
     }
 }
--- Assets/scripts/UI/PauseMenu.cs
+++ Assets/scripts/UI/PauseMenu.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     public static bool isPaused = false;
     [SerializeField] GameObject pauseMenu;
     [SerializeField] AudioSource onClickSound;
+    [SerializeField] GameController gameController;
+    [SerializeField] TextMeshProUGUI textDeath;
+    int i=0;
     private void Awake() {
         onClickSound.ignoreListenerPause=true;
+        gameController=GameObject.Find("main").GetComponent<GameController>();
+        textDeath.text="Deaths: " + PlayerPrefs.GetInt("deathNum");
     }
     private void Update() 
     {
@@ -41,7 +47,12 @@ public class PauseMenu : MonoBehaviour
         isPaused=false;
     }
     public void Quit()
-    {   onClickSound.Play();
-        Application.Quit();
+    {   
+        Time.timeScale=1f;
+        AudioListener.pause=false;
+        onClickSound.Play();
+        pauseMenu.SetActive(false);
+        gameController.LoadLevel(i);
+        isPaused=false;
     }
 }
