12ff4a8828042ef599ef83cfce91cac3ffa754fb
Hnah <hanh113004@gmail.com>
2023-09-07 13:37:05 +0700

    update for club day

--- Assets/scripts/obstacle map2/trapngocmai.cs
+++ Assets/scripts/obstacle map2/trapngocmai.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class trapngocmai : MonoBehaviour
 {
    public float Speed = 24f; 
-
+    public float stopPoint = 3.2f;
     private bool isFalling = false; 
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -18,7 +18,7 @@ public class trapngocmai : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (isFalling && transform.position.y >=3.1)
+        if (isFalling && transform.position.y >= stopPoint)
         {
             transform.Translate(Vector2.down * Speed * Time.fixedDeltaTime);
         }
